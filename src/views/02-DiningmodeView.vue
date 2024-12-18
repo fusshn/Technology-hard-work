3417cc4e71093d2903fb879d91491ac0b4971289
fusshn <kofiyang605@gmail.com>
2024-12-18 14:34:39 +0800

    新增到03

---
+++ src/views/02-DiningmodeView.vue
@@ -0,0 +1,52 @@
+<script></script>
+
+<template>
+ <div class="w-full h-screen max-w-[375px] font-semibold mx-auto flex flex-col">
+
+<nav class="h-[68px] p-[10px] flex justify-center items-center flex-col">
+    <a href="./01-起始畫面.html"><img src="./img/logo.png" alt="logo">
+    </a>
+</nav>
+
+
+<main class="h-full">
+    <img src="./img/image 1.png" alt="廣告圖">
+
+    <div class="h-[344px] py-[42px] flex justify-center flex-wrap gap-x-[12px]">
+        <h2 class="p-[10px]">請選擇用餐方式</h2>
+        <div class="p-[20px] flex gap-[20px]">
+            <div
+                class="card w-[140px] h-[160px] p-[8px] flex flex-col justify-center items-center border rounded-[5px] active:bg-[linear-gradient(144deg,_#FC6405_0%,_#E60013_103.82%)] active:text-white">
+                <a href="./03-點餐畫面.html" class="flex items-center flex-col">
+                    <p>內用</p>
+                    <img src="./img/內用.png" alt="內用圖">
+                </a>
+            </div>
+            <div
+                class="card w-[140px] h-[160px] p-[8px] flex flex-col justify-center items-center border rounded-[5px] active:bg-[linear-gradient(144deg,_#FC6405_0%,_#E60013_103.82%)] active:text-white">
+                <a href="./03-點餐畫面.html" class="flex items-center flex-col">
+                    <p>外帶</p>
+                    <img src="./img/外帶.png" alt="外帶圖">
+                </a>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="w-full h-[88px] flex bg-[#f5f5f5]">
+
+    <div class="w-[287px] h-full flex justify-center items-center gap-[12px] bg-[#f5f5f5]">
+        <button type="button"
+            class="w-[120px] h-[39px] flex justify-center items-center bg-[#fcc305] border-[none] rounded-[10px]">English</button>
+        <button type="button"
+            class="w-[120px] h-[39px] flex justify-center items-center bg-[#fcc305] border-[none] rounded-[10px]">友善模式</button>
+    </div>
+
+    <div class=" bg-[#f5f5f5] rounded-tr-[50%] rounded-br-[50%]">
+        <img src="./img/掃描.png" alt="QR掃描圖">
+    </div>
+
+</footer>
+
+</div>
+</template>
